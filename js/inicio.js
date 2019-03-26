fe753ecd9fea6e3bec68cf72d0d811b6b2b00b67
Pol Plana Ayet <MrPloman@MacBook-Pro-de-Pol.local>
2019-03-26 11:57:22 +0100

    primer commit amb tot

---
+++ js/inicio.js
@@ -0,0 +1,21 @@
+/*alert("Bienvenido a The Booker");
+alert("Aquí podrás encontrar libros de todo tipo");
+
+console.log("Bienvenido a la consola de The Booker");
+console.log(10+10); */
+
+/*var país="España";
+console.log(país);*/
+
+/*var continente="Europa";
+console.log(país, continente);*/
+
+/*var pais= "España";
+var continente= "Europa";
+var year= 2019;
+
+console.log(pais,continente,year);
+
+var pais_y_continente= pais+' '+continente;
+alert(pais_y_continente);*/
+
